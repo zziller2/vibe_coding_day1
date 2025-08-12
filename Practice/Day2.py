e5d3937a151adbf10ce00fb12b9b3fdb0f9c0d87
zziller2 <zziller2@gmail.com>
2025-08-12 21:07:06 +0900

    Add files via upload

---
+++ Practice/Day2.py
@@ -0,0 +1,17 @@
+title = "Vibe Coding Day 2"
+year = 2025
+price = 19.99
+skills = ["Python", "Data", "AI"]
+profile = {"username": "coder", "level": 1}
+
+print("학습 주제:", title)
+print("연도:", year)
+print("가격:", price)
+print("스킬 목록:", skills)
+print("프로필 정보:", profile)
+
+print(type(title))
+print(type(year))
+print(type(price))
+print(type(skills))
+print(type(profile))
---
+++ Practice/Day3.py
@@ -0,0 +1,19 @@
+score = 50
+
+if score >= 90:
+    print("A학점입니다.")   
+elif score >= 80:
+    print("B학점입니다.")
+elif score >= 70:
+    print("C학점입니다.")
+elif score >= 60:
+    print("D학점입니다.")
+else:
+    print("F학점입니다.")
+
+name = "영교"
+
+if name == "영석":
+    print("안녕하세요, 영석님!")
+if not name == "영석":
+    print("처음 오셨나요?")
